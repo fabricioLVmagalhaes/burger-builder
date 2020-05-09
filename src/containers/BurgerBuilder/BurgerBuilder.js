c9cf22b69568f42752182e0335386aa75eaa7500
Fabricio Magalhaes <fabricio.lvm@gmail.com>
2020-05-09 16:41:51 -0300

    Navigating to the Checkout Page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,29 +88,30 @@ class BugerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('here you go!!');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Fabricio Magalhaes',
-        address: {
-          street: 'Teststreet',
-          zipCode: '454545',
-          country: 'Germany',
-        },
-        email: 'slakfjlas@lkdfjl.com',
-      },
-      deliveryMethod: 'para ontem!',
-    };
-    axios
-      .post('/orders.json', order)
-      .then((response) => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Fabricio Magalhaes',
+    //     address: {
+    //       street: 'Teststreet',
+    //       zipCode: '454545',
+    //       country: 'Germany',
+    //     },
+    //     email: 'slakfjlas@lkdfjl.com',
+    //   },
+    //   deliveryMethod: 'para ontem!',
+    // };
+    // axios
+    //   .post('/orders.json', order)
+    //   .then((response) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch((error) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+    this.props.history.push('/checkout');
   };
 
   render() {
